5a89947bf6fd50a9d89f032c40d9043e74ddda9d
Ben Marshall <ben3005@hotmail.com>
2016-12-10 23:18:18 +0000

    Added two serialisations

---
+++ serialiser-benchmarks/sql-serialisation-benchmarker/DataReaderSerialisation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+
+namespace sql_serialisation_benchmarker
+{
+	public class DataReaderSerialisation
+	{
+		private static T SerialiseToObject<T>(SqlDataReader reader)
+			where T : new()
+		{
+			if (!reader.Read())
+			{
+				throw new ArgumentException("No data in the data reader", "reader");
+			}
+			if (reader.FieldCount == 0)
+			{
+				throw new ArgumentException("No columns in the reader", "reader");
+			}
+			T newObj = new T();
+			for (int i = 0; i < reader.FieldCount; i++)
+			{
+				setProperty(newObj, reader.GetName(i), reader.GetValue(i));
+			}
+			return newObj;
+		}
+
+		private static T SerialiseToObject<T>(IDataRecord dataRecord)
+			where T : new()
+		{
+			if (dataRecord.FieldCount == 0)
+			{
+				throw new ArgumentException("No columns in the dataRecord", "dataRecord");
+			}
+			T newObj = new T();
+			for (int i = 0; i < dataRecord.FieldCount; i++)
+			{
+				setProperty(newObj, dataRecord.GetName(i), dataRecord.GetValue(i));
+			}
+			return newObj;
+		}
+
+		private static IList<T> SerialiseToList<T>(SqlDataReader reader)
+			where T : new()
+		{
+			List<T> list = new List<T>();
+			if (!reader.Read())
+			{
+				throw new ArgumentException("No data in the data reader", "reader");
+			}
+			do
+			{
+				list.Add(SerialiseToObject<T>((IDataRecord)reader));
+			} while (reader.Read());
+			return list;
+		}
+
+		private static void setProperty<T>(T obj, string name, object value)
+			where T : new()
+		{
+			var propOfObj = obj.GetType().GetProperty(name, BindingFlags.Public | BindingFlags.Instance);
+			if (propOfObj != null)
+			{
+				if (propOfObj.PropertyType.IsEnum)
+				{
+					object[] parameters = new object[] { value.ToString(), null };
+					object result = typeof(Enum).GetMethods().Where(m => m.Name == "TryParse")
+						.Select(m => new { Method = m, Params = m.GetParameters(), Args = m.GetGenericArguments() })
+						.Where(x => x.Params.Length == 2 && x.Args.Length == 1 && x.Params[1].ParameterType.GetElementType() == x.Args[0])
+						.Select(x => x.Method)
+						.First()
+						.MakeGenericMethod(propOfObj.PropertyType)
+						.Invoke(null, parameters);
+
+					if ((bool)result)
+					{
+						propOfObj.SetValue(obj, Convert.ChangeType(parameters[1], propOfObj.PropertyType), null);
+					}
+				}
+				else
+				{
+					propOfObj.SetValue(obj, Convert.ChangeType(value, propOfObj.PropertyType));
+				}
+			}
+		}
+	}
+}
---
+++ serialiser-benchmarks/sql-serialisation-benchmarker/DataTableSerialisation.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+
+namespace sql_serialisation_benchmarker
+{
+	public class DataTableSerialization
+	{
+
+		/// <summary>
+		/// Serialises each row in a data table to an instance 
+		/// of the specified type
+		/// </summary>
+		/// <typeparam name="T">The type to create for each row</typeparam>
+		/// <param name="dataTable">The data to serialise</param>
+		/// <returns>A serialised list of items</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public static IList<T> SerialiseToList<T>(DataTable dataTable)
+			where T : new()
+		{
+			if (dataTable == null)
+			{
+				throw new ArgumentNullException("dataTable");
+			}
+			IList<T> list = new List<T>();
+
+			if (dataTable.Rows.Count > 0)
+			{
+				foreach (DataRow row in dataTable.Rows)
+				{
+					list.Add(SerialiseToObject<T>(row));
+				}
+			}
+
+			return list;
+		}
+
+		/// <summary>
+		/// Serializes the first row of a data table
+		/// to a specified object
+		/// </summary>
+		/// <typeparam name="T">The type to serialise the row to</typeparam>
+		/// <param name="dataTable">The data to serialise</param>
+		/// <returns>A serialised object with data from the first row</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public static T SerialiseToObject<T>(DataTable dataTable, int RowToSerialise = 0)
+			where T : new()
+		{
+			if (dataTable == null)
+			{
+				throw new ArgumentNullException("dataTable");
+			}
+			if (dataTable.Rows.Count < 1)
+			{
+				throw new ArgumentException("Must have at least one row in data table", "dataTable");
+			}
+			return SerialiseToObject<T>(dataTable.Rows[RowToSerialise]);
+		}
+
+		/// <summary>
+		/// Serialises a row into a specified object
+		/// by using the column names to set properties
+		/// </summary>
+		/// <typeparam name="T">The type to serialise the row to</typeparam>
+		/// <param name="dataRow">The row of data</param>
+		/// <returns>A serialised object form the row</returns>
+		/// <exception cref="ArgumentNullException"></exception>
+		/// <exception cref="ArgumentException"></exception>
+		public static T SerialiseToObject<T>(DataRow dataRow)
+			where T : new()
+		{
+			if (dataRow == null)
+			{
+				throw new ArgumentNullException("dataRow");
+			}
+			if (dataRow.Table == null)
+			{
+				throw new ArgumentException("Row is missing Table", "dataRow");
+			}
+
+			T obj = new T();
+
+			foreach (DataColumn column in dataRow.Table.Columns)
+			{
+				PropertyInfo prop = obj.GetType().GetProperty(column.ColumnName, BindingFlags.Public | BindingFlags.Instance);
+				if (prop != null && prop.CanWrite)
+				{
+					if (prop.PropertyType.IsEnum)
+					{
+						object[] parameters = new object[] { dataRow[column.ColumnName].ToString(), null };
+						object result = typeof(Enum).GetMethods().Where(m => m.Name == "TryParse")
+							.Select(m => new { Method = m, Params = m.GetParameters(), Args = m.GetGenericArguments() })
+							.Where(x => x.Params.Length == 2 && x.Args.Length == 1 && x.Params[1].ParameterType.GetElementType() == x.Args[0])
+							.Select(x => x.Method)
+							.First()
+							.MakeGenericMethod(prop.PropertyType)
+							.Invoke(null, parameters);
+
+						if ((bool)result)
+						{
+							prop.SetValue(obj, Convert.ChangeType(parameters[1], prop.PropertyType), null);
+						}
+					}
+					else
+					{
+						prop.SetValue(obj, Convert.ChangeType(dataRow[column.ColumnName], prop.PropertyType), null);
+					}
+				}
+			}
+			return obj;
+		}
+	}
+}
